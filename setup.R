aef69b8b7c36676d6fcb247769dc02d0ddee9691
Erin Grand <elg2158@columbia.edu>
2017-03-15 13:34:50 -0400

    setup lesson 1

---
+++ setup.R
@@ -0,0 +1,6 @@
+#Download packages and datasets
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+install.packages(c("tidyverse", "broom"))
+
+# set system langaue to english, just in case
+Sys.setenv(LANGUAGE = "en")
